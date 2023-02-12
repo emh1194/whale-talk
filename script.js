7776f15849c62c8cf01af06c256746d4d00f0445
Eric Hormuth <ehormuth94@gmail.com>
2023-02-12 16:23:31 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+let input = "turpentine and turtles";
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+const resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+    
+    if (input[i] == 'e' || input[i] == 'u') {
+        resultArray.push(input[i]);
+    }
+
+    for (let j = 0; j < vowels.length; j++) {
+        if (input[i] === vowels[j]) {
+            resultArray.push(input[i]);
+        }
+        
+    }
+    
+}
+
+const resultsString = resultArray.join("").toUpperCase();
+
+console.log(resultsString);
+
